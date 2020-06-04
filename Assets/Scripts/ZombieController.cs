d363a5a8e26bd8329e5cf08b79316fec6931792b
felipe <giraldofelipe95@gmail.com>
2020-06-04 17:30:11 -0500

    Initial commit

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Transform player;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        player = FindObjectOfType<player>().transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        agent.destination = player.position;
+        anim.SetFloat("speed",agent.velocity.magnitude / agent.speed);
+        
+    }
+}
