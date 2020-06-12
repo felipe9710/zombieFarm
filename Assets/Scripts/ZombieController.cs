509eb45f6252a1af2e9a04879c9280a1dec1e799
felipe <giraldofelipe95@gmail.com>
2020-06-12 18:18:02 -0500

    zombie ataque
    el zombie ataca cuando esta lo suficientemente cerca al player

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -8,22 +8,50 @@ public class ZombieController : MonoBehaviour
     NavMeshAgent agent;
     Transform player;
     Animator anim;
+    Transform zombie;
+
+    public float attackDistance = 0.5f;
+    private bool attacking;
+    public float attackduration = 1f;
+    public string attackAnimation;
+    public float attackTransition = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
-        player = FindObjectOfType<player>().transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        zombie = FindObjectOfType<ZombieController>().transform;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!attacking) {
+            agent.destination = player.position;
+            anim.SetFloat("speed", agent.velocity.magnitude / agent.speed);
 
-        agent.destination = player.position;
-        anim.SetFloat("speed",agent.velocity.magnitude / agent.speed);
-        
+            if(Vector3.Distance(player.position, zombie.position) <= attackDistance)
+            {
+                StartCoroutine(attackPlayer());
+            }
+        }
+        IEnumerator attackPlayer()
+        {
+            agent.isStopped = true;
+            attacking = true;
+            float d = 0f;
+            while (d < attackTransition)
+            {
+                d += Time.deltaTime;
+                yield return null;
+            }
+            anim.CrossFade(attackAnimation, 0.1f);
+            yield return new WaitForSeconds(attackduration);
+            attacking = false;
+            agent.isStopped = false;
+        }
     }
 }
