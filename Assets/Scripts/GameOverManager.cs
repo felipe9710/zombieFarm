d016f6aa2535495d3c6c33f9de183fc46d1bafd6
felipe <giraldofelipe95@gmail.com>
2020-06-30 00:24:55 -0500

    agregando game over

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Colision health;
+
+    Animator anim;
+
+    void Awake()
+    {
+
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (health.Health <= 0)
+        {
+            anim.SetTrigger("GameOver");
+        }
+        
+    }
+}
